7cdf4da59339aacd87c7f1087d858877fa68a6fa
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2022-07-26 11:40:45 -0300

    Script botões Prev e Next/ Trat. Not Found

--- js/script.js
+++ js/script.js
@@ -5,35 +5,72 @@ const pokemonImage = document.querySelector('.pokemon_image')
 const form = document.querySelector('.form')
 const input = document.querySelector('.input_search')
 
+const buttonPrev = document.querySelector('.btn-prev')
+const buttonNext = document.querySelector('.btn-next')
+
+let searchPokemon = 1;
+
 // Consumo da API POKEAPI
 const fetchPokemon = async (pokemon) => {
 
     // Usa o await para aguardar a resposta do fetch antes de prosseguir
     // OBS: O await só pode ser usado em uma função assincrona
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
-    // Pega o resultado da API e extrai o JSON
-    const data = await APIResponse.json();
-
-    return data;
-
+    // Verifica se o pokemon pesquisado foi encontrado
+    if (APIResponse.status === 200) {
+        // Pega o resultado da API e extrai o JSON
+        const data = await APIResponse.json();
+        return data;
+    }
 }
 
 // Função que vai renderizar os dados na tela
 const renderPokemon = async (pokemon) => {
 
+    // Coloca um valor na tela para que o usuario saiba que esta aguardando a busca
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = "";
+
+    // Aguarda o método de busca retornar um resultado da API
     const data = await fetchPokemon(pokemon);
 
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-
-    input.value = '';
-
+    // Se existirem dados no data então coloca na tela
+    if (data) {
+        pokemonImage.style.display = 'block';
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    
+        input.value = '';
+        searchPokemon = data.id;
+    } else {
+        pokemonImage.style.display = 'none';
+        pokemonName.innerHTML = "Not found";
+        pokemonNumber.innerHTML = "";
+    }
 }
 
 // Event Listener do formulario
 form.addEventListener('submit', (event) => {
+    // Evita o refresh da pagina no envio do form
     event.preventDefault();
-    renderPokemon(input.value);
+    // Chama a função que vai carregar o pokemon passando o valor do input
+    renderPokemon(input.value.toLowerCase());
 })
+
+// Event Listener do botão Prev
+buttonPrev.addEventListener('click', () => {
+    if (searchPokemon > 1) {
+        searchPokemon-=1;
+        renderPokemon(searchPokemon);
+    }
+})
+
+// Event Listener do botão Next
+buttonNext.addEventListener('click', () => {
+    searchPokemon+=1;
+    renderPokemon(searchPokemon);
+})
+
+renderPokemon(searchPokemon)
